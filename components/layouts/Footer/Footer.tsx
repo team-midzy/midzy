cc308e0cecd37eaa20498000c5673f224469f41b
MIDZY <support@midzy.kr>
2024-09-22 17:08:48 +0900

    💄 Footer 정렬 변경

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -4,19 +4,19 @@ const Footer = () => {
   const email = "support@midzy.kr";
 
   return (
-    <footer className="container py-6 text-sm font-light text-gray-600">
-      <section className="mb-3">
+    <footer className="container flex flex-wrap items-end justify-between py-6 text-xs font-light text-gray-600">
+      <section>
         <ITZYGalleryLogo />
 
-        <address className="mt-[2px] not-italic">
+        <address className="mt-1 not-italic">
           <a href={`mailto:${email}`}>{email}</a>
         </address>
       </section>
 
-      <article>
+      <article className="mt-3">
         <p>
-          &copy; <time>2024</time> 있지 믿지 날자 (aka ITZY Gallery Support
-          Team). All rights reserved.
+          &copy; <time>2024</time> 있지 믿지 날자 (aka ITZY 갤러리 서포트 팀).
+          All rights reserved.
         </p>
       </article>
     </footer>
