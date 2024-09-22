750365fafb94ccffbc1d6a065ba722b2bef09d1d
MIDZY <support@midzy.kr>
2024-09-22 16:46:06 +0900

    💄 컨테이너 기본패딩 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,13 @@ const config: Config = {
     },
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
     },
   },
   fontFamily: {
