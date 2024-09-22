e9a4f25cef41d797b602f293c6688386c3710f89
MIDZY <support@midzy.kr>
2024-09-22 11:27:50 +0900

    🎨 프리티어 기본 설정 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,24 @@
-/** @type {import("prettier").Options} */
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+
+  // 플러그인
   plugins: [
     require.resolve("@trivago/prettier-plugin-sort-imports"),
     require.resolve("prettier-plugin-tailwindcss"),
   ],
   tailwindFunctions: ["clsx"],
 };
+
+export default config;
