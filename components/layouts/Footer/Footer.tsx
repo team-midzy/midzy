610181350d58d44342e73723a9d8deee61d8f137
MIDZY <support@midzy.kr>
2024-09-25 23:38:57 +0900

    💄 배경색 변경에 따른 텍스트컬러 수정

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -4,21 +4,23 @@ const Footer = () => {
   const email = "support@midzy.kr";
 
   return (
-    <footer className="container flex flex-wrap items-end justify-between py-6 text-xs font-light text-neutral-600">
-      <section>
-        <ITZYGalleryLogo />
+    <footer className="flex flex-wrap items-end justify-between py-6 text-xs font-light text-neutral-400">
+      <div className="container">
+        <section>
+          <ITZYGalleryLogo />
 
-        <address className="mt-1 not-italic">
-          <a href={`mailto:${email}`}>{email}</a>
-        </address>
-      </section>
+          <address className="mt-1 not-italic">
+            <a href={`mailto:${email}`}>{email}</a>
+          </address>
+        </section>
 
-      <article className="mt-3">
-        <p>
-          &copy; <time dateTime="2024">2024</time> 있지 믿지 날자 (aka ITZY
-          갤러리 서포트 팀). All rights reserved.
-        </p>
-      </article>
+        <article className="mt-3">
+          <p>
+            &copy; <time dateTime="2024">2024</time> 있지 믿지 날자 (aka ITZY
+            갤러리 서포트 팀). All rights reserved.
+          </p>
+        </article>
+      </div>
     </footer>
   );
 };
