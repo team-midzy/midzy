fd9433bc660d09226619990e541722284a281d4e
MIDZY <support@midzy.kr>
2024-10-13 18:04:45 +0900

    💄 버튼 호버 효과 추가

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -11,8 +11,8 @@ const Button = ({
     type="button"
     className={twMerge(
       "rounded-full font-semibold transition duration-300 ease-in-out",
-      variant === "plain" && "bg-primary-500",
-      variant === "text" && "hover:text-itzy-500",
+      variant === "plain" && "bg-primary-500 hover:bg-primary-400",
+      variant === "text" && "hover:text-primary-500",
       size === "xs" && "h-6 px-2",
       size === "sm" && "h-7 px-2",
       size === "default" && "h-8 px-3",
